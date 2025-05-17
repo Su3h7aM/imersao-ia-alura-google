f80ccd07ff5f54004e15e3960711baf2c9ec2422
Su3h7aM <matheusgasource@gmail.com>
2025-05-17 02:05:38 -0300

    fix(tools): erro de digitação

--- wsagent/tools.py
+++ wsagent/tools.py
@@ -1,5 +1,5 @@
 from google.adk.tools import FunctionTool
 
-from wsagent.services import get_user_msg
+from wsagent.services import get_user_msgs
 
-user_msgs_tool = FunctionTool(func=get_user_msg)
+user_msgs_tool = FunctionTool(func=get_user_msgs)
